ca852929a7f260d98131122cf2c8c442b5109db3
RistoFlink <kenssi@gmail.com>
2023-09-05 16:19:39 +0300

    Initial commit

---
+++ src/test/java/dev/ristoflink/fieldvalidation/FieldValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ristoflink.fieldvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
