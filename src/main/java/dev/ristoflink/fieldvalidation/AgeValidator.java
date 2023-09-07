b7f588de131f0a7c0da4eb7f90848611c702c8c8
RistoFlink <kenssi@gmail.com>
2023-09-07 10:00:38 +0300

    Add logic for age validation

--- src/main/java/dev/ristoflink/fieldvalidation/AgeValidator.java
+++ src/main/java/dev/ristoflink/fieldvalidation/AgeValidator.java
@@ -1,13 +1,18 @@
 package dev.ristoflink.fieldvalidation;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
-public class AgeValidator implements ConstraintValidator<Age,int> {
-    long diff = new Date().getTime() - Age.getTime();
+public class AgeValidator implements ConstraintValidator<Age, Date> {
 
     @Override
-    public boolean isValid()
+    public boolean isValid(Date value, ConstraintValidatorContext context) {
+        long diff = new Date().getTime() - value.getTime();
+        int age = (int) (TimeUnit.MILLISECONDS.toDays(diff) / 365);
+        return age > 18;
+    }
 
 }
